ebd59c1e3e79f9899668e35adbb8e26c6c1f26d2
Scoll Stopper Secrets <learn@scollstoppersecrets.com>
2025-10-21 08:29:19 -0400

    Getters and Setters

--- OOP_Fundamentals/create_class_2.py
+++ OOP_Fundamentals/create_class_2.py
@@ -1,13 +1,27 @@
 class User:
-    def __init__(self, username, password):
+    def __init__(self, username, password, email):
         self.username = username
         self.password = password
+        self._email = email
+
+    def clean_email(self):
+        return self._email.lower().strip()
+    
+    def get_email(self):
+        return self._email
+    
+    def set_email(self, new_email):
+        self._email = new_email.lower().strip()
+
 
     def say_hi_to_user(self, user):
         print(f"Sending message to {user.username}: Hi {user.username}, it\'s {self.username}")
 
-user1 = User("JongWick", "3473974")
-user2 = User("Scooby", "338283")
+user1 = User("JongWick", "3473974", "  SkDdh@de.co")
+user2 = User("Scooby", "338283", "  TyeDamms@de.co")
+user3 = User("Boddy", "73737", "  SkDdh@de.co")
 
 user1.say_hi_to_user(user2)
 user2.say_hi_to_user(user1)
+
+print(user3.clean_email())
